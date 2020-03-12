004fa8c16b7a2736db8e2e1d01d7ddd650654665
nab98 <55541995+nab98@users.noreply.github.com>
2020-03-12 12:47:30 +0100

    1ere etape terminer

--- template/contact.inc.php
+++ template/contact.inc.php
@@ -1,20 +1,23 @@
 
 <section>
 	<h2>Contact</h2>
-		<form action="/contact.php" method="post">
-			<label for="nom">Votre Nom</label>
+		<form action="contact.php" method="post">
+			<!--<label for="Cursus">Cursus</label>-->
+			<input type="text" name="Cursus" id="Cursus" placeholder="Cursus">
+			<input type="text" name="Campus" id="Campus" placeholder="Campus">
+			<input type="text" name="niveau" id="niveau" placeholder="niveau">
+			<input type="text" name="formation" id="formation" placeholder="formation">
+			<!--<label for="nom">Nom</label>-->
 			<input type="text" name="nom" id="nom" placeholder="nom" required aria-required="true" autofocus>
 
-			<label for="prenom">Votre Prénom</label>
+			<!--<label for="prenom">Prénom</label>-->
 			<input type="text" name="nom" id="prenom" placeholder="prénom" required aria-required="true">
 
-			<label for="prenom">Votre Mail</label>
+			<!--<label for="prenom">Email</label>-->
 			<input type="email" name="mail" id="mail" placeholder="mail" required aria-required="true">
-			<label for="prenom">Votre message</label>
-			<!-- faut virer le text area c'est juste un type texte-->
-			<!--<textarea name="commentaire" id="commentaire" cols="30" rows="10">Commentaire</textarea> -->
-			<input type="text" name="commentaire" id="commentaire" placeholder="message" >
+			<!--<label for="prenom">Votre message</label>-->
+			<input type="text" name="telephone" id="telephone" placeholder="Telephone">
+			<input type="text" name="commentaire" id="commentaire" placeholder="niveau d'etudes actuel" >
 			<input type="submit" value="Envoyer">
-			
 		</form>
 </section>
