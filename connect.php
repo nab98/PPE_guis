5d59f3062fe68334eca4db464c23a7abde2d1968
nab98 <55541995+nab98@users.noreply.github.com>
2020-04-22 18:20:02 +0200

    try insertion bdd pdo

--- connect.php
+++ connect.php
@@ -1,4 +1,5 @@
 <?php
+//session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=ecole;charset=utf8', 'root', '');
 try
 {
@@ -9,12 +10,13 @@ catch (Exception $e)
 {
         die('Erreur : ' . $e->getMessage());
 }
+
     //require_once 'user2.php';
     //$newuser = new Inscription($login,$pass);
     //$newuser->getName();
     //$login = "Ledorf";
     //$pass = 2020;
-    class User{
+    /*class User{
         public $cursus;
         public $campus;
         public $niveau;
@@ -23,36 +25,48 @@ catch (Exception $e)
         public $prenom;
         public $mail;
         public $telephone;
-        public $etude;
+        public $etude;*/
 
-        //constructeur
-        public function __construct($cursus, $campus, $niveau, $formation, $nom, $prenom, $mail, $telephone, $etude)
+        
+        function construct()
         {
-                $this->cursus = $_POST['cursus'];
-                $this->campus= $_POST['campus'];
-                $this->niveau = $_POST['niveau'];
-                $this->formation = $_POST['formation'];
-                $this->nom = $_POST['nom'];
-                $this->prenom = $_POST['prenom'];
-                $this->mail = $_POST['mail'];
-                $this->telephone = $_POST['telephone'];
-                $this->etude = $_POST['commentaire'];
-
-
+              $cursus = $_POST['cursus'];
+              $campus= $_POST['campus'];
+              $niveau = $_POST['niveau'];
+              $formation = $_POST['formation'];
+              $nom = $_POST['nom'];
+              $prenom = $_POST['prenom'];
+              $mail = $_POST['mail'];
+              $telephone = $_POST['telephone'];
+              $etude = $_POST['commentaire'];
+              /* $cursus = array(":cursus"=>$_POST['cursus']);
+              $campus = array(":campus"=>$_POST['campus']);
+              $niveau = array(":niveau"=>$_POST['niveau']);
+              $formation = array(":formation"=>$_POST['formation']);
+              $nom = array(":$nom"=>$_POST['$nom']);
+              $prenom = array(":prenom"=>$_POST['prenom']);
+              $mail = array(":mail"=>$_POST['mail']);
+              $telephone = array(":telephone"=>$_POST['telephone']);
+              $etude = array(":etude"=>$_POST['etude']);*/
         }
-        //getter
-    
+    construct();
+    var_dump($_POST);
+   if (isset($_POST['Envoyer']))
+   {
+    $sql = $bdd->prepare('INSERT INTO eleves (cursus, campus, niveau , formation , nom , prenom , telephone , mail , etudes) VALUES ($cursus, $campus, $niveau, $formation, $nom, $prenom, $telephone, $mail, $etude)');
+     if(isset($_POST['cursus']) and isset($_POST['cursus']) and isset($_POST['cursus']) and isset($_POST['cursus']) and isset($_POST['cursus']) and isset($_POST['cursus']) and isset($_POST['cursus']) and isset($_POST['cursus']) and isset($_POST['cursus']))
+     {
+      construct();
+      $sql->execute();
+      if($sql)
+      {
+      echo 'Données insérées';
+      }
+      else
+      {
+        echo "Échec de l'opération d'insertion";
+      }
     }
-   if (isset($cursus) && isset($campus) && isset($niveau) && isset($formation) && isset($nom) && isset($prenom)  && isset($mail) && isset($telephone) && isset($etude))
-   {
-     $sql = $bdd->prepare("insert into 'eleves'('cursus','campus','niveau','formation','nom','prenom','telephone','mail','etudes') values ($cursus, $campus, $niveau, $formation, $nom, $prenom, $telephone, $mail, $etude)");
-     $sql-> execute();
-   }
-   if($sql)
-   {
-    echo 'Données insérées';
-  }else
-  {
-    echo "Échec de l'opération d'insertion";
-  }
+}
+      
 ?>
--- template/contact.inc.php
+++ template/contact.inc.php
@@ -1,23 +1,27 @@
 
 <section>
 	<h2>Contact</h2>
-		<form action="contact.php" method="post">
-			<!--<label for="Cursus">Cursus</label>-->
+		<form action="connect.php" method="post">
+			<label for="Cursus">Cursus</label>
 			<input type="text" name="Cursus" id="Cursus" placeholder="Cursus">
+			<label for="Campus">campus</label>
 			<input type="text" name="Campus" id="Campus" placeholder="Campus">
+			<label for="niveau">niveau</label>
 			<input type="text" name="niveau" id="niveau" placeholder="niveau">
+			<label for="formation">formation</label>
 			<input type="text" name="formation" id="formation" placeholder="formation">
-			<!--<label for="nom">Nom</label>-->
+			<label for="nom">Nom</label>
 			<input type="text" name="nom" id="nom" placeholder="nom" required aria-required="true" autofocus>
 
-			<!--<label for="prenom">Prénom</label>-->
+			<label for="prenom">Prénom</label>
 			<input type="text" name="nom" id="prenom" placeholder="prénom" required aria-required="true">
 
-			<!--<label for="prenom">Email</label>-->
+			<label for="email">Email</label>
 			<input type="email" name="mail" id="mail" placeholder="mail" required aria-required="true">
-			<!--<label for="prenom">Votre message</label>-->
+			<label for="telephone">telephone</label>
 			<input type="text" name="telephone" id="telephone" placeholder="Telephone">
+			<label for="commentaire">etude</label>
 			<input type="text" name="commentaire" id="commentaire" placeholder="niveau d'etudes actuel" >
-			<input type="submit" value="Envoyer">
+			<input type="submit" name ="Envoyer" value="Envoyer">
 		</form>
 </section>
